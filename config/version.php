b0c2a00f4fbf6005293bb0a7a6dfca5b82d55209
abilash <abi.sb123@gmail.com>
2020-05-22 18:43:28 +0530

    new commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Argon',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '0',
+
+    'patch'     =>  '12',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '21-May-2020',
+
+    'time'      =>  '19:00',
+
+    'zone'      =>  'GMT +3',
+
+];
